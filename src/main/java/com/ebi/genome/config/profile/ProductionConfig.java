2a42125a7471a8c15fe57261f4d1a780a67fea4d
manhtu <glmanhtu@gmail.com>
2017-11-05 15:23:15 +0100

    Fix refactor exception

--- src/main/java/com/ebi/genome/config/profile/ProductionConfig.java
+++ src/main/java/com/ebi/genome/config/profile/ProductionConfig.java
@@ -1,4 +1,4 @@
-package com.ebi.genome.config;
+package com.ebi.genome.config.profile;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
